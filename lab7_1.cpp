24915697afccc92ea301df54ee98615b5db08a8f
Godmonera <borwonpak_du@cmu.ac.th>
2022-12-13 15:31:52 +0700

    lab7 finished

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -34,8 +34,17 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+    string text ;
+    cout << "Input text: ";
+    cin >> text ;
+    cout << "Reversed text: " << func1(text)<< "\n";
+	if(func3(text)==func1(func3(text)))
+	{
+		 cout << "Palindrome: Yes" ;
+
+	} else{
+
+		 cout << "Palindrome: No" ;
+	}
     return 0;
 }
